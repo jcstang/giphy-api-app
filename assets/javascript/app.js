57d96d84b72d104da9544bf500aec049e1eaa6d4
Jacob Stanger <jstangwork@gmail.com>
2020-01-29 21:12:46 -0700

    changes around query strings and ratings.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $('button').on('click', function(){
   var daGif = $(this).attr('data-gif');
   // console.log('daGif ' + daGif);
 
-  var randomNumber = Math.floor(Math.random() * 500);
+  var randomNumber = Math.floor(Math.random() * 75);
   console.log(randomNumber);
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZrhJhYmel74Dmx4uq2zHvmOSxXpNlSgu&q=" +
     daGif.split(' ').join('+') + "&limit=10&offset=" + randomNumber + "&rating=G&lang=en";
@@ -22,12 +22,14 @@ $('button').on('click', function(){
   }).then(function(response){
 
     var results = response.data;
-    // console.log(results);
     var cardRowCount = 0;
-
+    
     // ====== loop thru the gifs that came back ==============================
     for (let i = 0; i < results.length; i++) {
       // cardRowCount += 1;
+      // console.log(results);
+      console.log(results[i].title);
+      console.log(results[i].rating);
 
       if (cardRowCount <= 4) {
         var parentDiv = $('<div>').addClass('col-sm-4');
@@ -37,6 +39,9 @@ $('button').on('click', function(){
         // ===== Title ================
         // var cardTitle = $('<h5>').text('put json title thing here');
         var cardTitle = $('<h5>').text(results[i].title);
+        if (results[i].title === '') {
+          cardTitle.text('GIF GIPHY GIF');
+        }
         var cardHeader = $('<div>').addClass('card-header text-wrap').append(cardTitle);
         cardDiv.append(cardHeader);
   
@@ -45,8 +50,8 @@ $('button').on('click', function(){
         var cardBody = $('<div>').addClass('card-body').append(cardImg);
         cardDiv.append(cardBody);
 
-        console.log('here is cardDiv');
-        console.log(cardDiv);
+        // console.log('here is cardDiv');
+        // console.log(cardDiv);
         
         // TODO: add this cardDiv to the holding place
         // $('.first-row').append(cardDiv);
