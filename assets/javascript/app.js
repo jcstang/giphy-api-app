312a0e3045e002576bc2cc541545df409b7a06d5
Jacob Stanger <jstangwork@gmail.com>
2020-01-29 21:24:27 -0700

    creating button function to get ready for user input.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,16 +22,13 @@ $('button').on('click', function(){
   }).then(function(response){
 
     var results = response.data;
-    var cardRowCount = 0;
     
     // ====== loop thru the gifs that came back ==============================
     for (let i = 0; i < results.length; i++) {
-      // cardRowCount += 1;
       // console.log(results);
       console.log(results[i].title);
       console.log(results[i].rating);
 
-      if (cardRowCount <= 4) {
         var parentDiv = $('<div>').addClass('col-sm-4');
         var cardDiv = $('<div>').addClass('card mb-4 shadow-sm mx-auto');
         parentDiv.append(cardDiv);
@@ -50,27 +47,22 @@ $('button').on('click', function(){
         var cardBody = $('<div>').addClass('card-body').append(cardImg);
         cardDiv.append(cardBody);
 
-        // console.log('here is cardDiv');
-        // console.log(cardDiv);
-        
-        // TODO: add this cardDiv to the holding place
-        // $('.first-row').append(cardDiv);
         $('.first-row').prepend(cardDiv);
-
-        // TODO: how do I make a new row after 3 gifs
-
-      } 
-      // else {
-      //   cardRowCount = 0;
-      // }
-
-      // console.log('cardRowCount: ' + cardRowCount);
-
 
     }
 
   });
 
+
+  // TODO: use this function to help with attaching a new button from user input
+  function createButton(text) {
+    var newButton = $('<button>')
+      .addClass('btn btn-info')
+      .attr('data-gif', text)
+      .text(text);
+    
+    $('.buttons-row').append(newButton);
+  }
 
 
 
