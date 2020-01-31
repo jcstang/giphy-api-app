352aa63a2492832b621e6779522c7b36559f4bcd
Jacob Stanger <jstangwork@gmail.com>
2020-01-30 17:08:19 -0700

    removed other event listener. working now!!!!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -93,59 +93,59 @@ $(document).ready(function () {
   }
 
 
-  $('.animal-button').on('click', function() {
+  // $('.animal-button').on('click', function() {
   
-    var daGif = $(this).attr('data-gif');
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZrhJhYmel74Dmx4uq2zHvmOSxXpNlSgu&q=" + 
-    daGif.split(' ').join('+') + "&limit=10&offset=" + randomNumber + "&rating=G&lang=en";
+  //   var daGif = $(this).attr('data-gif');
+  //   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZrhJhYmel74Dmx4uq2zHvmOSxXpNlSgu&q=" + 
+  //   daGif.split(' ').join('+') + "&limit=10&offset=" + randomNumber + "&rating=G&lang=en";
 
     
-    // ===================================================
-    // AJAX CALL - to giphy api
-    // ===================================================
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).then(function(response){
+  //   // ===================================================
+  //   // AJAX CALL - to giphy api
+  //   // ===================================================
+  //   $.ajax({
+  //     url: queryURL,
+  //     method: 'GET'
+  //   }).then(function(response){
   
-      var results = response.data;
+  //     var results = response.data;
       
-      // ====== loop thru the gifs that came back ==============================
-      for (let i = 0; i < results.length; i++) {
-        // console.log(results);
-        console.log(results[i].title);
-        console.log(results[i].rating);
+  //     // ====== loop thru the gifs that came back ==============================
+  //     for (let i = 0; i < results.length; i++) {
+  //       // console.log(results);
+  //       console.log(results[i].title);
+  //       console.log(results[i].rating);
   
-          var parentDiv = $('<div>').addClass('col-sm-4 card-deck');
-          var cardDiv = $('<div>').addClass('card mb-4 shadow-sm mx-auto border-primary');
-          parentDiv.append(cardDiv);
-  
-    
-          // ===== Title ================
-          // var cardTitle = $('<h5>').text('put json title thing here');
-          var cardTitle = $('<h5>').text(results[i].title);
-          if (results[i].title === '') {
-            cardTitle.text('GIF GIPHY GIF');
-          }
-          var cardHeader = $('<div>').addClass('card-header text-wrap border-primary').append(cardTitle);
-          cardDiv.append(cardHeader);
-    
-          // ====== card gif img =========
-          var cardImg = $('<img>').attr('src', results[i].images.fixed_height.url);
-          var cardBody = $('<div>').addClass('card-body').append(cardImg);
-          cardDiv.append(cardBody);
-  
-          $('.first-row').prepend(cardDiv);
-  
-      }
-  
-    });
-  
+  //         var parentDiv = $('<div>').addClass('col-sm-4 card-deck');
+  //         var cardDiv = $('<div>').addClass('card mb-4 shadow-sm mx-auto border-primary');
+  //         parentDiv.append(cardDiv);
   
     
+  //         // ===== Title ================
+  //         // var cardTitle = $('<h5>').text('put json title thing here');
+  //         var cardTitle = $('<h5>').text(results[i].title);
+  //         if (results[i].title === '') {
+  //           cardTitle.text('GIF GIPHY GIF');
+  //         }
+  //         var cardHeader = $('<div>').addClass('card-header text-wrap border-primary').append(cardTitle);
+  //         cardDiv.append(cardHeader);
+    
+  //         // ====== card gif img =========
+  //         var cardImg = $('<img>').attr('src', results[i].images.fixed_height.url);
+  //         var cardBody = $('<div>').addClass('card-body').append(cardImg);
+  //         cardDiv.append(cardBody);
+  
+  //         $('.first-row').prepend(cardDiv);
+  
+  //     }
+  
+  //   });
+  
+  
     
     
-  });
+    
+  // });
   
   // ===================================================
   // CUSTOM FUNCTIONS
