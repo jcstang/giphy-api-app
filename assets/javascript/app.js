686b220384afc3cf9aa23ff610c77f804deda669
Jacob Stanger <jstangwork@gmail.com>
2020-01-30 17:06:47 -0700

    made ajax its own function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -54,6 +54,45 @@ $(document).ready(function () {
   });
 
 
+  function getTheGifsFromGiphy() {
+    var daGif = $(this).attr('data-gif');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZrhJhYmel74Dmx4uq2zHvmOSxXpNlSgu&q=" + 
+      daGif.split(' ').join('+') + "&limit=10&offset=" + randomNumber + "&rating=G&lang=en";
+    
+    $.ajax({
+       url: queryURL,
+       method: 'GET'
+    }).then(function(response){
+      var results = response.data;
+
+      for (let i = 0; i < results.length; i++) {
+  
+          var parentDiv = $('<div>').addClass('col-sm-4 card-deck');
+          var cardDiv = $('<div>').addClass('card mb-4 shadow-sm mx-auto border-primary');
+          parentDiv.append(cardDiv);
+  
+          // ===== Title ================
+          // var cardTitle = $('<h5>').text('put json title thing here');
+          var cardTitle = $('<h5>').text(results[i].title);
+          if (results[i].title === '') {
+            cardTitle.text('GIF GIPHY GIF');
+          }
+          var cardHeader = $('<div>').addClass('card-header text-wrap border-primary').append(cardTitle);
+          cardDiv.append(cardHeader);
+    
+          // ====== card gif img =========
+          var cardImg = $('<img>').attr('src', results[i].images.fixed_height.url);
+          var cardBody = $('<div>').addClass('card-body').append(cardImg);
+          cardDiv.append(cardBody);
+  
+          $('.first-row').prepend(cardDiv);
+  
+      }
+
+    });
+  }
+
+
   $('.animal-button').on('click', function() {
   
     var daGif = $(this).attr('data-gif');
@@ -154,5 +193,11 @@ $(document).ready(function () {
   // prepend to view
   // get movie title
   // append to movie container
+
+
+  // ===================================================
+  // global like event listener on the buttons
+  // ===================================================
+  $(document).on('click', '.animal-button', getTheGifsFromGiphy);
 
 });
